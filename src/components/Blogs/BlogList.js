bb0c2eed47591ce45272dace14beb10d41da5bb1
Sanket Sutar <sanketsutar010@gmail.com>
2026-01-10 19:22:58 +0530

    Added new BlogCard

---
+++ src/components/Blogs/BlogList.js
@@ -0,0 +1,30 @@
+
+//To handle data + mapping 
+
+import React from "react";
+import { BlogCard } from "./BlogCard";
+import { blogPosts } from "../../data/blogData";
+
+const BlogList = () => {
+  return (
+    <div className="card-container">
+      {blogPosts.map(
+        ({ id, image, category, date, title, description, userId, userImg ,userName}) => (
+          <BlogCard
+            key={id}
+            userImg={userImg}
+            userName={userName}
+            userId={userId}
+            image={image}
+            category={category}
+            date={date}
+            title={title}
+            description={description}
+          />
+        )
+      )}
+    </div>
+  );
+};
+
+export default BlogList;
---
+++ src/data/blogData.js
@@ -0,0 +1,33 @@
+export const blogPosts = [
+
+  {
+    id: 1,
+    userImg: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=200",
+    userName: "CSESA SGI",
+    userId:
+      "https://www.instagram.com/csesa_sgi?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+    image:
+      "https://images.unsplash.com/photo-1519389950473-47ba0277781c?w=800",
+    category: "TECH",
+    date: "5–6 min read",
+    title: "Understanding System Design Basics",
+    description:
+      "A beginner-friendly guide to system design concepts and interview preparation."
+  },
+  {
+    id: 2,
+    userImg: "https://images.unsplash.com/photo-1527980965255-d3b416303d12?w=200",
+    userName: "CSESA SGI",
+    userId:
+      "https://www.instagram.com/csesa_sgi?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+    image:
+      "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?w=800",
+    category: "CLUB NEWS",
+    date: "3 min read",
+    title: "CSESA Inauguration Highlights",
+    description:
+      "A glimpse into the inauguration event featuring workshops, speakers, and student activities."
+  }
+];
+
+
